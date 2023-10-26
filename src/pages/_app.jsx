8b7fb80596070b642f5fa825e48afdcf54ac5104
Alvon Jovanus Chandra <alvonchan@gmail.com>
2023-10-26 12:00:31 +0700

    add change my password in auth

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -24,9 +24,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {router.pathname === '/' ? (
-          <title>Protocol API Reference</title>
+          <title>Pin Point API Reference</title>
         ) : (
-          <title>{`${pageProps.title} - Protocol API Reference`}</title>
+          <title>{`${pageProps.title} - Pin Point API Reference`}</title>
         )}
         <meta name="description" content={pageProps.description} />
       </Head>
